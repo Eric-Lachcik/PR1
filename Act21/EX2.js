03987424191566d0d582ac166bb3766b328d0700
Eric-Lachcik <lachcikeric@gmail.com>
2023-12-05 17:12:33 +0100

    Ex2 acabado

--- Act21/EX2.js
+++ Act21/EX2.js
@@ -3,10 +3,19 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const scan = readline.createInterface({ input, output });
 
-scan.question("Introdueix una temperatura " , (answer) =>{
-    scan.question("Es C o F?", (answer) =>{
-        
-    })
-
-
+scan.question("Introdueix una temperatura: " , (Temperatura) =>{
+    scan.question("Es C o F?", (answer1) =>{
+        let centi, fare;  
+        if((answer1) == "C"){
+            centi = (Temperatura);
+            fare = (9/5*centi)+32;
+            console.log("Tu temperatura en Fahrenheit es " + fare )
+        }
+        else if((answer1) == "F"){
+            fare = (Temperatura);
+            centi = (fare-32)*(5/9);
+            console.log("Tu temperatura en Celsius es " + centi)
+        }
+    scan.close();
+    });
 })
