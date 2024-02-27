eb8b5c0d59d561a4e655bafa6295908a9c920f4f
Eric Lachcik <lachcikeric@gmail.com>
2024-02-27 19:38:45 +0100

    NavBar

--- act33/src/Components/Navbar.tsx
+++ act33/src/Components/Navbar.tsx
@@ -1,40 +1,44 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 
-const navbar = () => {
+function NavBar() {
     return <>
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <Link to='/'>
+        {[false].map((expand) => (
+            <Navbar expand="sm" style={{backgroundColor: '#50C878'}} >
+                <Container fluid>
+                    <Navbar.Brand as={Link} to='/'>
                         <img src="./Knekro.png" width='70'></img>
-                    </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav mx-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link " to='/'>Inicio</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link " to= '/puntos'>Puntos de Interes</Link>
-                            </li>
-                            <li className="nav-item">
-                               <Link className="nav-link " to= '/restaurante'>Restaurantes</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link " to= '/hotel'>Hoteles</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link " to= '/actividad'>Actividades</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+                    </Navbar.Brand>
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar-expand-${expand}`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                CIDE
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-center flex-grow-1 pe-3 ">
+                                <Nav.Link as={Link} to='/' >Inicio</Nav.Link>
+                                <Nav.Link as={Link} to='/puntos'>Puntos de Interes</Nav.Link>
+                                <Nav.Link as={Link} to='/hotel'>Hoteles</Nav.Link>
+                                <Nav.Link as={Link} to='/restaurante'>Restaurantes</Nav.Link>
+                                <Nav.Link as={Link} to='/actividad'>Actividades</Nav.Link>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                </Container>
+            </Navbar>
+        ))}
     </>
 }
-export default navbar;
+
+export default NavBar;
