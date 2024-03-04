ea84c177c2f0d1a93442a577471abcbcb3028cb4
Eric Lachcik <lachcikeric@gmail.com>
2024-03-04 20:04:58 +0100

    Puntos de Interes

--- act33/src/Views/Inicio.tsx
+++ act33/src/Views/Inicio.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+
+import Image from 'react-bootstrap/Image';
+
 
 
 
 function Inicio(){
     return<>
-        <div className="Inicio">
-            <h1>Bienvenidos al colegio Cide</h1>
-            <p>Somos un colegio que formamos una cooperativa donde cada uno aporta su granito de arena</p>
-        </div>
+        <Container fluid className="mt-3">   
+            <div className="">
+                <h1>Bienvenidos al colegio Cide</h1>
+                <p>Somos un colegio que formamos una cooperativa donde cada uno aporta su granito de arena</p>
+                <Image src="./cideInicio.png" roundedCircle/>
+            </div>
+        </Container>   
     </>
 }
 export default Inicio;
--- act33/src/Views/Puntos.tsx
+++ act33/src/Views/Puntos.tsx
@@ -1,8 +1,48 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 function Puntos(){
     return<>
-        <h1>Puntos de Interes</h1>
+       <Container fluid className="mt-3 mb-3">
+            <h1>Puntos de Interés</h1>
+       </Container>
+       <Container fluid className="mt-4">
+       <Row>
+                <Col md={4} className="mb-4">
+                    <Card>
+                        <Card.Img variant="top" src="ruta/a/tu/imagen1.jpg" />
+                        <Card.Body>
+                            <Card.Text>
+                                Este es un texto corto para el primer punto.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} className="mb-4">
+                    <Card>
+                        <Card.Img variant="top" src="ruta/a/tu/imagen2.jpg" />
+                        <Card.Body>
+                            <Card.Text>
+                                Este es un texto corto para el segundo punto.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} className="mb-4">
+                    <Card>
+                        <Card.Img variant="top" src="ruta/a/tu/imagen3.jpg" />
+                        <Card.Body>
+                            <Card.Text>
+                                Este es un texto corto para el tercer punto.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+       </Container>
     </>
 }
 export default Puntos;
