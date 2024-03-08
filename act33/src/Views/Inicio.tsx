f1eb85846f2655f7794715652322f1d1d8426be8
Eric Lachcik <lachcikeric@gmail.com>
2024-03-08 20:33:48 +0100

    Por acabar

--- act33/src/Views/Inicio.tsx
+++ act33/src/Views/Inicio.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
-
 import Image from 'react-bootstrap/Image';
+import inicio from "../Images/Inicio.png";
 
 
 
@@ -12,7 +12,7 @@ function Inicio() {
             <div className="">
                 <h1>Bienvenidos al colegio Cide</h1>
                 <p>Somos un colegio que formamos una cooperativa donde cada uno aporta su granito de arena</p>
-                <Image src="./cideInicio.png" roundedCircle />
+                <Image src={inicio} roundedCircle />
             </div>
         </Container>
     </>
