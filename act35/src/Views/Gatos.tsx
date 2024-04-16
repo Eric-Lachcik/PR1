f087ed985c026816c86648e36e13a0bf7eb9a06c
Eric Lachcik <lachcikeric@gmail.com>
2024-04-16 18:12:54 +0200

    DataJson modificado

--- act35/src/Views/Gatos.tsx
+++ act35/src/Views/Gatos.tsx
@@ -7,26 +7,46 @@ import Col from 'react-bootstrap/Col';
 export type Gatitos = Gato[]
 
 export interface Gato {
-  Nombre: string
-  Dulzura: number
-  Disponible: boolean
-  Imagen: string
+    Nombre: string
+    Dulzura: number
+    Disponible: boolean
+    Imagen: string
 }
 
 
-function Gatos(){
-    const[Gatitos,setGatitos] = React.useState ([] as Gatitos)
+function Gatos() {
+    const [Gatitos, setGatitos] = React.useState([] as Gatitos)
 
     React.useEffect(() => {
-      fetch("https://raw.githubusercontent.com/pedrogarciaaag/Lenguaje-de-marcas/main/actividad_35/data.json")
-        .then((response)=>response.json())
-        .then((data:Gatitos)=>{
-            setGatitos(data)
-        }) 
+        fetch("https://raw.githubusercontent.com/Eric-Lachcik/PR1/main/act35/gatitos.json")
+            .then((response) => response.json())
+            .then((data: Gatitos) => {
+                setGatitos(data)
+            })
     }, [])
-    
-    
-    return<></>
+
+
+    return <>
+        <div className="m-2">
+            <Row xs={1} md={3} className="g-4">
+                {Gatitos.map(Gatitos => (
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={Gatitos.Imagen} style={{ height: '350px', objectFit: 'cover' }} />
+                            <Card.Body>
+                                <Card.Title>{Gatitos.Nombre}</Card.Title>
+                                <Card.Text className="text-background m-2" >
+                                    Nivel de dulzura: {Gatitos.Dulzura}
+                                </Card.Text>
+                                <Card.Text className="text-background m-2" >
+                                    Esta disponible?:{Gatitos.Disponible ? 'Sí' : 'No'}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </div></>
 }
 
 export default Gatos;
