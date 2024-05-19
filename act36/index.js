ee8d57e04af22d493cf4256d6d944c728ea75064
Eric-Lachcik <Lachcikeric@gmail.com>
2024-05-19 23:46:00 +0200

    Prueba de funcionalidad

--- act36/index.js
+++ act36/index.js
@@ -53,6 +53,59 @@ app.get('/comanda', (req, res) => {
    
     res.json(resultado)
 })
+app.post('/usuari', (req, res) => {
+    const { nom, email } = req.body;
+
+    
+    if (!nom || !email) {
+        return res.status(400).json({ message: 'Nombre y email son campos obligatorios' });
+    }
+
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'Formato de email incorrecto' });
+    }
+
+    const insertUser = db.prepare('INSERT INTO usuarios (nom, email) VALUES (?, ?)');
+    const result = insertUser.run(nom, email);
+
+    if (result.changes > 0) {
+        res.status(201).json({ message: 'Usuario creado' });
+    } else {
+        res.status(500).json({ message: 'Usuario no creado' });
+    }
+})
+
+app.get('/addusuario', (req, res) => {
+    res.render('addusuarios');
+})
+
+app.post('/producte', (req, res) => {
+    const { nom, preu } = req.body;
+
+    if (!nom || nom.trim() === '') {
+        return res.status(400).json({ message: 'El nombre es obligatorio' });
+    }
+
+    const preuNumber = parseFloat(preu);
+    if (isNaN(preuNumber) || preuNumber <= 0) {
+        return res.status(400).json({ message: 'El precio debe ser un número positivo' });
+    }
+
+    const insertProducte = db.prepare('INSERT INTO productos (nom, preu) VALUES (?, ?)');
+    const result = insertProducte.run(nom, preu);
+
+    if (result.changes > 0) {
+        res.status(201).json({ message: 'Producto creado' });
+    } else {
+        res.status(500).json({ message: 'Producto no creado' });
+    }
+})
+
+app.get('/addproducto', (req, res) => {
+    res.render('addproductes');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
